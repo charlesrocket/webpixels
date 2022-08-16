e7477f9c8c55ac7c414ceb299c97982deb78ae28
-k <slowdive@me.com>
2022-08-16 16:04:38 -0700

    Add `webpixels` reference

--- lib/tests/web.rs
+++ lib/tests/web.rs
@@ -3,19 +3,21 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
+use webpixels::{pixelmosh, Options};
+
 pub mod files;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn valid_image() {
-    let options = webpixels::Options::default();
-    webpixels::pixelmosh(&files::VALID_IMAGE.to_vec(), &options).unwrap();
+    let options = Options::default();
+    pixelmosh(&files::VALID_IMAGE.to_vec(), &options).unwrap();
 }
 
 #[wasm_bindgen_test]
 fn invalid_image() {
-    let options = webpixels::Options::default();
-    let result = webpixels::pixelmosh(&files::INVALID_IMAGE.to_vec(), &options);
+    let options = Options::default();
+    let result = pixelmosh(&files::INVALID_IMAGE.to_vec(), &options);
     assert!(result.is_err());
 }
