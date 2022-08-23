643043e99a7bddd1e98342b273bff441ba55f4ae
-k <slowdive@me.com>
2022-08-22 18:41:41 -0700

    Add `image_view`

--- www/src/main.rs
+++ www/src/main.rs
@@ -9,6 +9,7 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
         drop_zone_active: false,
         drop_zone_content: vec![div!["PNG DROP ZONE"]],
         file_texts: Vec::new(),
+        image_view: "".to_string(),
     }
 }
 
@@ -16,6 +17,7 @@ struct Model {
     drop_zone_active: bool,
     drop_zone_content: Vec<Node<Msg>>,
     file_texts: Vec<String>,
+    image_view: String,
 }
 
 enum Msg {
@@ -34,6 +36,7 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
         Msg::Drop(file_list) => {
             model.drop_zone_active = false;
             model.file_texts.clear();
+            model.image_view.clear();
 
             let files = (0..file_list.length())
                 .map(|index| file_list.get(index).unwrap())
@@ -60,7 +63,7 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
                     );
 
                     log!("PIXELMOSH: DONE", file.name());
-                    Msg::FileView(new_array) //;
+                    Msg::FileView(new_array)
                 });
             }
         }
@@ -76,8 +79,7 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
             .unwrap();
 
             let url = web_sys::Url::create_object_url_with_blob(&blob).unwrap();
-            let window = web_sys::window().unwrap();
-            window.open_with_url(&url).unwrap();
+            model.image_view = url;
         }
     }
 }
@@ -103,50 +105,55 @@ macro_rules! stop_and_prevent {
 }
 
 fn view(model: &Model) -> Node<Msg> {
-    div![div![
-        style![
-            St::Height => "auto",
-            St::Width => "auto",
-            St::Margin => "auto",
-            St::Background => if model.drop_zone_active { "lightgreen" } else { "orange" },
-            St::FontFamily => "monospace",
-            St::Color => "black",
-            St::FontSize => "42px",
-            St::Padding => "13px",
-            St::FontWeight => "bold",
-            St::Display => "flex",
-            St::FlexDirection => "column",
-            St::JustifyContent => "center",
-            St::AlignItems => "center",
-            St::Border => [&px(7), "dashed", "black"].join(" ");
-        ],
-        ev(Ev::DragEnter, |event| {
-            stop_and_prevent!(event);
-            Msg::DragEnter
-        }),
-        ev(Ev::DragOver, |event| {
-            let drag_event = event.into_drag_event();
-            stop_and_prevent!(drag_event);
-            drag_event.data_transfer().unwrap().set_drop_effect("copy");
-            Msg::DragOver
-        }),
-        ev(Ev::DragLeave, |event| {
-            stop_and_prevent!(event);
-            Msg::DragLeave
-        }),
-        ev(Ev::Drop, |event| {
-            let drag_event = event.into_drag_event();
-            stop_and_prevent!(drag_event);
-            let file_list = drag_event.data_transfer().unwrap().files().unwrap();
-            Msg::Drop(file_list)
-        }),
+    div![
         div![
-            style! {
-                St::PointerEvents => "none",
-            },
-            model.drop_zone_content.clone(),
+            style![
+                St::Height => "auto",
+                St::Width => "auto",
+                St::Margin => "auto",
+                St::Background => if model.drop_zone_active { "lightgreen" } else { "orange" },
+                St::FontFamily => "monospace",
+                St::Color => "black",
+                St::FontSize => "42px",
+                St::Padding => "13px",
+                St::FontWeight => "bold",
+                St::Display => "flex",
+                St::FlexDirection => "column",
+                St::JustifyContent => "center",
+                St::AlignItems => "center",
+                St::Border => [&px(7), "dashed", "black"].join(" ");
+            ],
+            ev(Ev::DragEnter, |event| {
+                stop_and_prevent!(event);
+                Msg::DragEnter
+            }),
+            ev(Ev::DragOver, |event| {
+                let drag_event = event.into_drag_event();
+                stop_and_prevent!(drag_event);
+                drag_event.data_transfer().unwrap().set_drop_effect("copy");
+                Msg::DragOver
+            }),
+            ev(Ev::DragLeave, |event| {
+                stop_and_prevent!(event);
+                Msg::DragLeave
+            }),
+            ev(Ev::Drop, |event| {
+                let drag_event = event.into_drag_event();
+                stop_and_prevent!(drag_event);
+                let file_list = drag_event.data_transfer().unwrap().files().unwrap();
+                Msg::Drop(file_list)
+            }),
+            div![
+                style! {
+                    St::PointerEvents => "none",
+                },
+                model.drop_zone_content.clone(),
+            ],
         ],
-    ]]
+        img![attrs! {
+            At::Src => model.image_view
+        },],
+    ]
 }
 
 pub fn main() {
