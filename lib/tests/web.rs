acc88992e9c2e5ac3ac8d006fd13b23941ed1fea
-k <slowdive@me.com>
2022-11-30 20:39:22 -0800

    Fix tests

--- lib/tests/web.rs
+++ lib/tests/web.rs
@@ -12,13 +12,13 @@ wasm_bindgen_test_configure!(run_in_browser);
 #[wasm_bindgen_test]
 fn valid_image() {
     let options = Options::default();
-    let result = pixelmosh(&files::VALID_IMAGE.to_vec(), &options);
+    let result = pixelmosh(&images::VALID_IMAGE.to_vec(), &options);
     assert!(!result.is_err());
 }
 
 #[wasm_bindgen_test]
 fn invalid_image() {
     let options = Options::default();
-    let result = pixelmosh(&files::INVALID_IMAGE.to_vec(), &options);
+    let result = pixelmosh(&images::INVALID_IMAGE.to_vec(), &options);
     assert!(result.is_err());
 }
